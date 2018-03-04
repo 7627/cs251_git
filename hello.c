492cf6afd05b52e05e31fd5263413e7c13f541cf
Amit yadav <yamit@iitk.ac.in>
2018-03-05 00:24:30 +0530

    step 24 done

--- hello.c
+++ hello.c
@@ -1,20 +1,8 @@
 #include<stdio.h>
-<<<<<<< HEAD
 void main(){
   printf("Helloworld!\n");
   printf("This must be a monolithic design\n");
-=======
-void microkernel_sendmsg(char *);
-void microkernel_getmsg(char *);
-void main(){
-    printf("Helloworld!\n");
-    printf("This must be a monolithic design\n");
-    microkernel_sendmsg("is more portable");
-}
-void microkernel_sendmsg(char *a){
-  printf("microkernel: %s\n", a);
 }
 void microkernel_getmsg(char *b){
   //TODO: getmsg feature
->>>>>>> feature_addition_getmsg
 }
